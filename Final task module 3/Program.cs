123f59200e798262f9c9c4283f99fdda4b27b3c1
Evgeny40rus <hat-99@mail.ru>
2021-03-09 19:30:51 +0300

    Добавлена строка:  о просьбе ввести возраст. Строка вывода имени и возраста.

--- Final task module 3/Program.cs
+++ Final task module 3/Program.cs
@@ -5,9 +5,13 @@
     {
         public static void Main(string[] args)
         {
-        Console.Write("Введите имя");
+        Console.Write("Введите имя:");
         var name = Console.ReadLine();
+
+        Console.Write("Введите возраст:");
         var age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя {0} и Ваш возраст {1}", name, age);
 
         }
     }
