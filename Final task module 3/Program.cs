94990d1f67cdcd23d73ed191277c50ca513dfc93
Evgeny40rus <hat-99@mail.ru>
2021-03-09 19:24:50 +0300

    Добавлена строка "Введите имя" Строка прочтении введенного имени. Строка получени прочтения возраста.

--- Final task module 3/Program.cs
+++ Final task module 3/Program.cs
@@ -5,7 +5,10 @@
     {
         public static void Main(string[] args)
         {
-            
+        Console.Write("Введите имя");
+        var name = Console.ReadLine();
+        var age = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
